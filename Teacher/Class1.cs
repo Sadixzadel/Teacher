38d7f7acf8a395867fa55a2fefc684b090e2f5b4
HP <HP@DESKTOP-EQ9MNTC>
2022-10-02 01:26:22 +0400

    Добавьте файлы проекта.

---
+++ Teacher/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1;
+
+class Teacher
+{
+    public string Name;
+    public string Subject;
+    public decimal Salary;
+    public decimal additionalSalary;
+
+    public Teacher(string name, string subject, decimal salary)
+    {
+        Name = name;
+        Subject = subject;
+        Salary = salary;
+    }
+
+    public void IncreaseSalary(decimal additionalSalary)
+    {
+        additionalSalary = 200;
+        this.Salary += additionalSalary;
+    }
+
+    // public override string ToString()
+    // {
+    // Return his representation as String
+    //   var builder = new StringBuilder();
+
+    // builder.Append($"{Name , Salary , Salary} ");   //?
+
+    //return builder.ToString();
+
+    //}
+}
+
---
+++ Teacher/Program.cs
@@ -0,0 +1,27 @@
+﻿using Homework1;
+
+string name;
+string subject;
+decimal salary;
+
+
+Console.Write("Enter teacher's name: ");
+name = Convert.ToString(Console.ReadLine());
+
+Console.Write("Enter teacher's subject: ");
+subject = Convert.ToString(Console.ReadLine());
+
+Console.Write("Enter teacher's salary: ");
+salary = Convert.ToDecimal(Console.ReadLine());
+
+
+var teacher = new Teacher(name, subject, salary);
+
+
+//var newSalary = teacher.IncreaseSalary();  //?
+//teacher.Salary = newSalary;               //?
+
+
+
+
+Console.WriteLine($"Name: {name}  Subject: {subject}   Salary: {salary}");
